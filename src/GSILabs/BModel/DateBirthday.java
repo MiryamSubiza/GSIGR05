e7fb86bff2b960ff089531982b12c4a44400b5ad
MiryamSubiza <miryam_247@hotmail.com>
2015-09-22 19:13:48 +0200

    Clase para insertar fecha de nacimiento en Cliente

---
+++ src/GSILabs/BModel/DateBirthday.java
@@ -0,0 +1,52 @@
+/*
+ * This document is part of the lab material for the subject:
+ * Gestion de Sistemas de Informacion
+ * to be released at the
+ * Universidad Publica de Navarra
+ * during the first semester of the Academic Year 2015-2016
+ */
+package GSILabs.BModel;
+
+/**
+ * 
+ * @author subiza.79082
+ * @author izu.78236
+ * @version 1.0 (18/09/2015)
+ */
+public class DateBirthday {
+    
+    public int day; //1 <= day <= 31
+    public int month; //1 <= month <= 12
+    public int year; //1900 <= year <= 2015
+    
+    public DateBirthday (int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+    
+    public void setDay (int day) {
+        this.day = day;
+    }
+    
+    public int getDay () {
+        return day;
+    }
+    
+    public void setMonth (int month) {
+        this.month = month;
+    }
+    
+    public int getMonth () {
+        return month;
+    }
+    
+    public void setYear (int year) {
+        this.year = year;
+    }
+    
+    public int getYear () {
+        return year;
+    }
+    
+}

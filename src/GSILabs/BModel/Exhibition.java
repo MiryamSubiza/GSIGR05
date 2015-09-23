1a1c039d628b2516b3e78e4fb0dd6ce5346195db
aizu04 <alexizucarmona@gmail.com>
2015-09-23 20:15:17 +0200

    He implementado todos los metodos de la interfaz aunque no estoy muy seguro con el metodo de si el performer esta en la exibicion esta bien hecho o no

--- src/GSILabs/BModel/Exhibition.java
+++ src/GSILabs/BModel/Exhibition.java
@@ -59,7 +59,7 @@ public class Exhibition implements LastingEvent, Event{
         return organizerName;
     }
     
-    public void setStartDateExhibition (Date startDateExhibition) {
+    public void setStartDate (Date startDateExhibition) {
         this.startDateExhibition = startDateExhibition;
     }
     
@@ -71,7 +71,7 @@ public class Exhibition implements LastingEvent, Event{
         this.closingDateExhibition = closingDateExhibition;
     }
     
-    public Date getClosingDateExhibition () {
+    public Date getEndingDate () {
         return closingDateExhibition;
     }
     
@@ -104,7 +104,7 @@ public class Exhibition implements LastingEvent, Event{
     }
     
     public Date[] getDates(){
-        Date[] datesExhibition;
+        Date[] datesExhibition = null;
         Date auxDate = startDateExhibition;
         for(int i=0;i<(calcularDiasExibicion(startDateExhibition, closingDateExhibition));i++){
             datesExhibition[i] = auxDate;
@@ -112,6 +112,98 @@ public class Exhibition implements LastingEvent, Event{
         }
         return datesExhibition;
     }
+    
+    private int calcularDiasExibicion(Date dia1, Date dia2){
+        
+        int numDias = 0; // Variable a devolver que contendra el numero de dias de diferencia entre una fecha y otra
+        if((dia2.getMonth() - dia1.getMonth()) >= 1){
+
+            numDias = 30*(dia2.getMonth() - dia1.getMonth() - 1) + dia2.getDay() + (numDiasMes(dia1.getMonth()) - dia1.getDay());
+           
+        }
+        else{
+            numDias = dia2.getDay() - dia1.getDay();
+        }
+        
+        return numDias;
+    }
+    
+    private int numDiasMes (int mes){
+        int nDias;
+        switch(mes){
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                nDias = 31;
+                break;
+            case 4: case 6: case 9: case 11:
+                nDias = 30;
+                break;
+            case 2:
+                nDias = 28;
+                break;
+            default:
+                nDias = 30;
+        }
+        
+        return nDias;
+    }
+    
+    private Date aumentarDia(Date dia){
+        Date proximoDia = null;
+        switch (dia.getMonth()){
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                if(dia.getDay() == 31){
+                    if(dia.getMonth() == 12){
+                        proximoDia = new Date(dia.getYear()+1, 1, 1);
+                    }
+                    else{
+                        proximoDia = new Date(dia.getYear(), dia.getMonth() + 1, 1);
+                    }
+                }
+                else{
+                    proximoDia = new Date(dia.getYear(), dia.getMonth(), dia.getDay() + 1);
+                }
+                break;
+                
+            case 4: case 6: case 9: case 11:
+                if(dia.getDay() == 30){
+                    proximoDia = new Date(dia.getYear(), dia.getMonth() + 1, 1);
+                }
+                else{
+                    proximoDia = new Date(dia.getYear(), dia.getMonth(), dia.getDay() + 1);
+                }
+                break;
+                
+            case 2:
+                if(dia.getDay() == 28){
+                    proximoDia = new Date(dia.getYear(), dia.getMonth() + 1, 1);
+                }
+                else{
+                    proximoDia = new Date(dia.getYear(), dia.getMonth(), dia.getDay() + 1);
+                }
+                break;
+        }
+        
+        return proximoDia;
+    }
+    
+    public Performer[] getPerformers(){
+        
+        Performer[] performers = null;
+        performers[0] = p;
+        return performers;
+        
+    }
+    
+    public boolean involvesPerformer(Performer p){
+        
+        if(p.getName().equalsIgnoreCase(this.p.getName()))
+            return true;
+        else{
+            return false;
+        }
+        
+    }
+    
     
     // HAY QUE IMPLEMENTAR LOS DOS METODOS INVENTADOS DE ARRIBA
     

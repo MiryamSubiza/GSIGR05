b9efade51a8e21be36461bde97702d26a9c163b4
MiryamSubiza <miryam_247@hotmail.com>
2015-09-24 18:15:20 +0200

    Implementa todos los métodos de los interfaces ImpermanentEvent y Event

--- src/GSILabs/BModel/Concert.java
+++ src/GSILabs/BModel/Concert.java
@@ -20,11 +20,11 @@ public class Concert implements ImpermanentEvent {
     
     private String concertName; //Nombre asociado con el evento
     private Performer p; //Puede ser Artista o Colectivo, pero solamente uno
-    private Date startDateConcert; //Fecha del concierto
+    private Date startDateConcert; //Fecha del concierto (un solo día)
     private Date startTimeConcert; //Hora de comienzo
     private Date doorOpeningTimeConcert; //Hora de apertura de puertas
     private Date closingTimeConcert; //Hora de cierre
-    private Location location;
+    private Location location; //Esta localización es única
     
     public Concert (String concertName, Performer p, Date startDateConcert, 
             Date startTimeConcert, Date doorOpeningTimeConcert, 
@@ -119,34 +119,47 @@ public class Concert implements ImpermanentEvent {
     }
 
     //Sobreescribe el método de Event
+    //p es el performer que tenemos almacenado en la clase Concert
+    //pAskedFor es el performer por el que preguntan si actúa en el concierto
     @Override
-    public boolean involvesPerformer (Performer pEvent) {
+    public boolean involvesPerformer (Performer pAskedFor) {
 
         //Utilización del operador instanceof para determinar si el tipo de cierto objeto Performer es Artist
         if (p instanceof Artist) {
-            //Conversión descendente de la referencia de Performer
-            //a una referencia de Artist
-            Artist a = (Artist) p;
+            //Conversión descendente de la referencia de Performer a una referencia de Artist
+            Artist aInvolved = (Artist) p;
             
-            if (a.equals
-            
+            //Preguntan por un artista y tenemos un artista, comparación por nombre
+            if (pAskedFor instanceof Artist) {
+                Artist aAskedFor = (Artist) pAskedFor;
+                if (aInvolved.equals(aAskedFor)) return true;
+                else return false;
+            }
+            //Preguntan por un colectivo y tenemos un artista, directamente no está involucrado
+            else if (pAskedFor instanceof Collective) return false;
+            else return false;
         }
-        
-    }
-    
-    //Devuelve true si dos artistas son iguales y false si no son iguales
-    public boolean equals (Artist a2) {
-        
-        if (this.p instanceof Artist) {
-            Artist a1 = (Artist)this.p;
-            if ((a1.getName().equalsIgnoreCase(a2.getName())) && 
-                    (a1.getWorkDescription().equalsIgnoreCase(a2.getWorkDescription())) &&
-                    (a1.getWebSite().equalsIgnoreCase(a2.getWorkDescription()))) return true;
+        else if (p instanceof Collective) {
+            Collective cInvolved = (Collective) p;
+            
+            //Preguntan por un artista y tenemos un colectivo, mirar si el artista está en el colectivo
+            if (pAskedFor instanceof Artist) {
+                Artist aAskedFor = (Artist) pAskedFor;
+                if (cInvolved.isArtistInCollective(aAskedFor)) return true;
+                else return false;
+                
+            }
+            //Preguntan por un colectivo y tenemos un colectivo, comparación por nombre
+            else if (pAskedFor instanceof Collective) {
+                Collective cAskedFor = (Collective) pAskedFor;
+                if (cInvolved.equals(cAskedFor)) return true;
+                else return false;
+            }
             else return false;
         }
         else return false;
         
-    }   
+    }
     
     //Sobreescribe el método de Event
     @Override
@@ -161,15 +174,3 @@ public class Concert implements ImpermanentEvent {
     }
     
 }
-
-        // Conversión descendente de la referencia de Employee
-        // a una referencia de BasePlusCommissionEmployee
-        BasePlusCommissionEmployee employee =
-                                (BasePlusCommissionEmployee) currentEmployee ;
-
-        double oldBaseSalary = employee.getBaseSalary();
-        employee.setBaseSalary( 1.10 * oldBaseSalary );
-        System.out.printf(
-                "new base salary with 10%% increase is: $%,.2f\n",
-                                                                employee.getBaseSalary() );
-} // end if

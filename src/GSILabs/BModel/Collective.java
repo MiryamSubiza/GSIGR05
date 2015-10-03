090c3811ced50163ca902ba1abc4a876354fce67
MiryamSubiza <miryam_247@hotmail.com>
2015-10-03 10:16:13 +0200

    Añadido javadoc

--- src/GSILabs/BModel/Collective.java
+++ src/GSILabs/BModel/Collective.java
@@ -8,25 +8,31 @@
 
 package GSILabs.BModel;
 
-//import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 
 /**
- * 
+ * Información sobre colectivos, este tipo de performers son asociados a eventos
  * @author subiza.79082
  * @author izu.78236
- * @version 1.0 (21/09/2015)
+ * @version 03/10/2015
  */
 public class Collective implements Performer {
     
     //Ha de ser una colección sin ordenación -> HashSet no tiene ordenación y no permite duplicados
     private HashSet <Artist> artists; // Todos los artistas que componen el colectivo
-    private String name; // Nombre del colectivo de artistas
-    private String workDescription; // Descripción del colectivo de artistas
-    private String webSite = "-"; // Página web de dicho colectivo de artistas
+    private String name;
+    private String workDescription;
+    private String webSite = "-";
     
-    //Colectivo con página web
+    /**
+     * Primer método constructor, inicialización de variables para un colectivo
+     * con página web
+     * @param firstArtist Primer artista que compone el colectivo
+     * @param name Nombre del colectivo de artistas
+     * @param workDescription Descripción del colectivo de artistas
+     * @param webSite Página web
+     */
     public Collective (Artist firstArtist, String name, String workDescription, String webSite) {
         
         artists = new HashSet();
@@ -37,7 +43,13 @@ public class Collective implements Performer {
         
     }
     
-    //Colectivo sin página web
+    /**
+     * Primer método constructor, inicialización de variables para un colectivo
+     * sin página web
+     * @param firstArtist Primer artista que compone el colectivo
+     * @param name Nombre del colectivo de artistas
+     * @param workDescription Descripción del colectivo de artistas
+     */
     public Collective (Artist firstArtist, String name, String workDescription) {
         
         artists = new HashSet();
@@ -47,90 +59,113 @@ public class Collective implements Performer {
         
     }
     
+    /**
+     * Añadir un artista al colectivo de artistas
+     * @param newArtist Artista a añadir
+     */
     public void addArtistToCollective (Artist newArtist) {
         artists.add(newArtist);
     }
     
+    /**
+     * Establecer nombre del colectivo
+     * @param name Nombre del colectivo
+     */
     public void setName (String name) {
         this.name = name;
     }
     
-    //Sobreescribe el método de Performer
+    /**
+     * Establecer descripción del colectivo de artistas
+     * @param description Descripción del colectivo de artistas
+     */
     @Override
     public void setWorkDescription (String description) {
         this.workDescription = description;
     }
     
+    /**
+     * Establecer página web
+     * @param webSite Página web
+     */
     public void setWebSite (String webSite) {
         this.webSite = webSite;
     }
     
-    //Sobreescribe el método de Performer
+    /**
+     * Obtener nombre artístico
+     * @return Nombre artístico
+     */
     @Override
     public String getName() {
         return name;
     }
     
-    //Sobreescribe el método de Performer
+    /**
+     * Obtener descripción del colectivo de artistas
+     * @return Descripción del colectivo de artistas
+     */
     @Override
     public String getWorkDescription(){
         return workDescription;
     }
     
+    /**
+     * Obtener página web
+     * @return página web
+     */
     public String getWebSite () {
         return webSite;
     }
     
-    /* Devuelve verdadero si el artista se encuentra en el colectivo
-    *  y falso en caso contrario
-    */
+    /**
+     * Comprobar si un artista se encuentra en este colectivo
+     * @return True si el artista se encuentra en el colectivo. False en caso contrario
+     */
     public boolean isArtistInCollective (Artist auxArtist) {
         
         Iterator i = artists.iterator();
         Artist artist = null;
         while (i.hasNext()) {
             artist = (Artist)i.next();
-            if (auxArtist.equals(artist)) {
-                break;
-            }
+            if (auxArtist.equals(artist)) break;
         }
-        if (auxArtist.equals(artist)) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return auxArtist.equals(artist);
         
     }
     
-    // Elimina el artista del HashSet de artistas
+    /**
+     * Eliminar un artista del colectivo
+     * @param a Artista a eliminar
+     * @return True si el artista ha sido eliminado. False en caso contrario
+     */
     public boolean removeArtist(Artist a){
-        
-        if(artists.contains(a)){
-            // Si el HashSet de artistas contiene dicho artista
-            // lo elimino
-            return artists.remove(a);
-        }
-        else{
-            // El artista no esta en el HashSet
-            return false;
-        }
-        
+        // Si el HashSet de artistas contiene dicho artista lo elimino
+        if(artists.contains(a)) return artists.remove(a);
+        // El artista no esta en el HashSet
+        else return false;        
     }
     
-    //Devuelve true si dos colectivos son iguales y false si no son iguales
+    /**
+     * Comparación entre dos objetos Coletivo
+     * @param o Objeto a comparar
+     * @return True si tienen el mismo nombre. False en caso contrario
+     */
     @Override
     public boolean equals (Object o) {
         
         if (o instanceof Collective) {
             Collective c = (Collective)o;
-            if (this.getName().equalsIgnoreCase(c.getName())) return true;
-            else return false;
+            return this.getName().equalsIgnoreCase(c.getName());
         }
         else return false;
 
     }
     
+    /**
+     * Representación por pantalla
+     * @return Información a mostrar
+     */
     @Override
     public String toString () {
         return "COLLECTIVE\nName: " + name + "\nWork description: " + workDescription + 

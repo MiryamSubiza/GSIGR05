bfb3692cb2deb380745324385569ae21d9803f71
MiryamSubiza <miryam_247@hotmail.com>
2015-10-03 10:17:36 +0200

    Añadido javadoc

--- src/GSILabs/BModel/Exhibition.java
+++ src/GSILabs/BModel/Exhibition.java
@@ -13,25 +13,38 @@ import java.util.Date;
 import java.util.HashSet;
 
 /**
- * 
+ * Evento protagonizado por un artista o varios de ellos, puede extenderse durante
+ * varios días o semanas
  * @author subiza.79082
  * @author izu.78236
- * @version 1.0 (21/09/2015)
+ * @version 03/10/2015
  */
 public class Exhibition implements LastingEvent {
     
-    /* La exposición puede extenderse durante varios días o semanas */
-    private String exhibitionName; //Nombre de la exposición
-    private String title; //Título de la exposición
-    private String organizerName; //Nombre de la entidad organizadora
-    private FechasHoras startDateExhibition; //Fecha de apertura
-    private FechasHoras closingDateExhibition; //Fecha de cierre
-    private FechasHoras startTimeExhibition; //Hora de apertura
-    private FechasHoras closingTimeExhibition; //Hora de cierre
-    private Performer p; //Puede ser un artista o varios, en cuyo caso se considerará colectivo
-    private HashSet <String> webLinks; //Enlaces web (número indeterminado)
-    private Location location; //Esta localización es única
+    private String exhibitionName;
+    private String title;
+    private String organizerName;
+    private FechasHoras startDateExhibition;
+    private FechasHoras closingDateExhibition;
+    private FechasHoras startTimeExhibition;
+    private FechasHoras closingTimeExhibition;
+    private Performer p;
+    private HashSet <String> webLinks;
+    private Location location;
     
+    /**
+     * Método constructor, inicialización de variables
+     * @param exhibitionName Nombre asociado con el evento
+     * @param title Título de la exposición
+     * @param organizerName Nombre de la entidad organizadora
+     * @param startDateExhibition Fecha de apertura
+     * @param closingDateExhibition Fecha de cierre
+     * @param startTimeExhibition Hora de apertura
+     * @param closingTimeExhibition Hora de cierre
+     * @param p Performer, puede ser un artista o varios, en cuyo caso se considerará colectivo
+     * @param webLink Enlaces web (número indeterminado)
+     * @param location Localización única
+     */
     public Exhibition (String exhibitionName, String title, String organizerName, 
             FechasHoras startDateExhibition, FechasHoras closingDateExhibition, FechasHoras startTimeExhibition,
             FechasHoras closingTimeExhibition, Performer p, String webLink, Location location) {
@@ -50,85 +63,165 @@ public class Exhibition implements LastingEvent {
         
     }
     
+    /**
+     * Establecer nombre de la exposición
+     * @param exhibitionName Nombre de la exposición
+     */
     public void setExhibitionName (String exhibitionName) {
         this.exhibitionName = exhibitionName;
     }
     
+    /**
+     * Obtener nombre de la exposición
+     * @return Nombre de la exposición
+     */
     @Override
     public String getName () {
         return exhibitionName;
     }
     
+    /**
+     * Establecer título de la exposición
+     * @param title Título de la exposición
+     */
     public void setTitle (String title) {
         this.title = title;
     }
     
+    /**
+     * Obtener título de la exposición
+     * @return Título de la exposición
+     */
     public String getTitle () {
         return title;
     }
     
+    /**
+     * Establecer entidad organizadora
+     * @param organizerName Entidad organizadora
+     */
     public void setOrganizerName (String organizerName) {
         this.organizerName = organizerName;
     }
     
+    /**
+     * Obtener entidad organizadora
+     * @return Entidad organizadora
+     */
     public String getOrganizerName () {
         return organizerName;
     }
     
+    /**
+     * Establecer fecha de apertura
+     * @param startDateExhibition Fecha de apertura
+     */
     public void setStartDateExhibition (FechasHoras startDateExhibition) {
         this.startDateExhibition = startDateExhibition;
     }
     
+    /**
+     * Obtener fecha de apertura
+     * @return Fecha de apertura
+     */
     @Override
     public Date getStartDate () {
         return startDateExhibition;
     }
     
+    /**
+     * Establecer fecha de cierre
+     * @param closingDateExhibition Fecha de cierre
+     */
     public void setClosingDateExhibition (FechasHoras closingDateExhibition) {
         this.closingDateExhibition = closingDateExhibition;
     }
     
+    /**
+     * Obtener fecha de cierre
+     * @return Fecha de cierre
+     */
     @Override
     public Date getEndingDate () {
         return closingDateExhibition;
     }
     
+    /**
+     * Establecer hora de apertura
+     * @param startTimeExhibition Hora de apertura
+     */
     public void setStartTimeExhibition (FechasHoras startTimeExhibition) {
         this.startTimeExhibition = startTimeExhibition;
     }
     
+    /**
+     * Obtener hora de apertura
+     * @return Hora de apertura
+     */
     public Date getStartTimeExhibition () {
         return startTimeExhibition;
     }
     
+    /**
+     * Establecer hora de cierre
+     * @param closingTimeExhibition Hora de cierre
+     */
     public void setClosingTimeExhibition (FechasHoras closingTimeExhibition) {
         this.closingTimeExhibition = closingTimeExhibition;
     }
     
+    /**
+     * Obtener hora de cierre
+     * @return Hora de cierre
+     */
     public Date getClosingTimeExhibition () {
         return closingTimeExhibition;
     }
     
+    /**
+     * Establecer performer
+     * @param p Performer, un artista o varios, en cuyo caso se considerará colectivo
+     */
     public void setPerformer (Performer p) {
         this.p = p;
     }
     
+    /**
+     * Obtener performer
+     * @return Performer, un artista o varios, en cuyo caso se considerará colectivo
+     */
     public Performer getPerformer () {
         return p;
     }
     
+    /**
+     * Añadir enlace web a la exposición
+     * @param webLink Enlace web
+     */
     public void addWebLink (String webLink) {
         webLinks.add(webLink);
     }
     
+    /**
+     * Establecer localización
+     * @param location Location
+     */
     public void setLocation (Location location) {
         this.location = location;
     }
     
+    /**
+     * Obtener localización
+     * @return Localización única
+     */
     public Location getLocation () {
         return location;
     }
     
+    /**
+     * Obtener días que abarca la exposición
+     * @return Días que dura la exposición
+     */
     @Override
     public Date[] getDates() {
         ArrayList <Date> al = new ArrayList();
@@ -140,21 +233,26 @@ public class Exhibition implements LastingEvent {
         return (Date[]) al.toArray();
     }
     
+    /**
+     * Obtener cuántos días hay de diferencia entre una fecha y otra
+     * @return Número de días de diferencia entre una fecha y otra
+     */
     private int calculateExhibitionDays (FechasHoras dia1, FechasHoras dia2) {
         
-        // Variable a devolver que contendra el numero de dias de diferencia entre una fecha y otra
         int numDias;
         if ((dia2.getMes() - dia1.getMes()) >= 1) {
             numDias = 30*(dia2.getMes() - dia1.getMes() - 1) + dia2.getDia() + 
                     (numDiasMes(dia1.getMes()) - dia1.getDia());
         }
-        else {
-            numDias = dia2.getDia() - dia1.getDia();
-        }
-        
+        else numDias = dia2.getDia() - dia1.getDia();
         return numDias;
     }
     
+    /**
+     * Obtener cuántos días contiene un determinado mes
+     * @param mes Mes a calcular
+     * @return Número de días que contiene ese mes
+     */
     private int numDiasMes (int mes) {
         int nDias;
         switch(mes){
@@ -173,56 +271,52 @@ public class Exhibition implements LastingEvent {
         
         return nDias;
     }
-    
-    // El método coge la fecha que se le pasa como argumento y
-    // aumenta un día (cambiando si es necesario el mes o año de la fecha)
+
+    /**
+     * Incrementar una fecha un día (cambiando si es necesario el mes o año de la fecha)
+     * @param day Día a incrementar
+     * @return Día incrementado
+     */
     private FechasHoras incrementDay (FechasHoras day) {
         FechasHoras nextDay = null;
         switch (day.getMes()){
             case 1: case 3: case 5: case 7: case 8: case 10: case 12:
                 if(day.getDia() == 31){
-                    if(day.getMes() == 12){
-                        nextDay = new FechasHoras(1, 1, day.getAnio()+1, day.getHora(), day.getMinuto());
-                    }
-                    else{
-                        nextDay = new FechasHoras(1, day.getMes() + 1, day.getAnio(), day.getHora(), day.getMinuto());
-                    }
+                    if(day.getMes() == 12) nextDay = new FechasHoras(1, 1, day.getAnio()+1, day.getHora(), day.getMinuto());
+                    else nextDay = new FechasHoras(1, day.getMes() + 1, day.getAnio(), day.getHora(), day.getMinuto());
                 }
-                else{
-                    nextDay = new FechasHoras(day.getDay() + 1, day.getMes(), day.getAnio(), day.getHora(), day.getMinuto());
-                }
+                else nextDay = new FechasHoras(day.getDay() + 1, day.getMes(), day.getAnio(), day.getHora(), day.getMinuto());
                 break;
                 
             case 4: case 6: case 9: case 11:
-                if(day.getDia() == 30){
-                    nextDay = new FechasHoras(1, day.getMes() + 1, day.getAnio(), day.getHora(), day.getMinuto());
-                }
-                else{
-                    nextDay = new FechasHoras(day.getDia() + 1, day.getMes(), day.getAnio(), day.getHora(), day.getMinuto());
-                }
+                if(day.getDia() == 30) nextDay = new FechasHoras(1, day.getMes() + 1, day.getAnio(), day.getHora(), day.getMinuto());
+                else nextDay = new FechasHoras(day.getDia() + 1, day.getMes(), day.getAnio(), day.getHora(), day.getMinuto());
                 break;
                 
             case 2:
-                if(day.getDia() == 28){
-                    nextDay = new FechasHoras(1, day.getMes() + 1, day.getAnio(), day.getHora(), day.getMinuto());
-                }
-                else{
-                    nextDay = new FechasHoras(day.getDia() + 1, day.getMes(), day.getAnio(), day.getHora(), day.getMinuto());
-                }
+                if(day.getDia() == 28) nextDay = new FechasHoras(1, day.getMes() + 1, day.getAnio(), day.getHora(), day.getMinuto());
+                else nextDay = new FechasHoras(day.getDia() + 1, day.getMes(), day.getAnio(), day.getHora(), day.getMinuto());
                 break;
-        }
-        
+        }        
         return nextDay;
     }
     
+    /**
+     * Comprobar si en esta exposición actúa un performer dado
+     * @param p Performer por el que se pregunta
+     * @return True si el performer forma parte de la exposición. False en caso contrario
+     */
     @Override
     public boolean involvesPerformer (Performer p) {
         
-        if (p.equals(this.p)) return true;
-        else return false;
+        return p.equals(this.p);
         
     }
     
+    /**
+     * Obtener los performers que actúan en este evento
+     * @return Performers que actúan en este evento
+     */
     @Override
     public Performer[] getPerformers() {
         
@@ -232,26 +326,34 @@ public class Exhibition implements LastingEvent {
         
     }
     
+    /**
+     * Comparación entre dos objetos Exhibition
+     * @param o Objeto a comparar
+     * @return True si se llaman de la misma manera. False en caso contrario
+     */
     @Override
     public boolean equals (Object o) {
         
         if (o instanceof Exhibition) {
             Exhibition e = (Exhibition)o;
-            if (this.getName().equalsIgnoreCase(e.getName())) return true;
-            else return false;
+            return this.getName().equalsIgnoreCase(e.getName());
         }
         else return false;
         
     }
     
+    /**
+     * Representación por pantalla
+     * @return Información a mostrar
+     */
     @Override
     public String toString() {
         return "EXHIBITION\nExhibition's name: " + exhibitionName + "\nTitle: " +
                 title + "\nOrganizer name: " + organizerName + "\nStart date: " + 
-                startDateExhibition + "\nStart time: " + 
-                startTimeExhibition + "h\nClosing date: " + 
-                closingDateExhibition + "\nClosing time: " + 
-                closingTimeExhibition+ "h\nPerformer: " + p.getName() + 
+                startDateExhibition.fechaToString() + "\nStart time: " + 
+                startTimeExhibition.horaToString() + "h\nClosing date: " + 
+                closingDateExhibition.fechaToString() + "\nClosing time: " + 
+                closingTimeExhibition.horaToString() + "h\nPerformer: " + p.getName() + 
                 "\nLocation: " + location.getName() + "\n";
     }
     
